1f3dcf956d1c438c73f4d87c45b6ff93e8abc266
cfr3yt <160527793+cfr3yt@users.noreply.github.com>
2024-03-29 16:19:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalid = null;
 
+initializeSlider(){
+    
+function initializeSlider(){
+
+    slides[slideIndex].classList.add("displaySlide");
+
+}
+function showSlide(index){
+
+}
+function prevSlide(){
+
+}
+function nextSlide(){
+
+}
