1da83f94e40c8f8e3f1998848638b8f1d8796b18
cfr3yt <160527793+cfr3yt@users.noreply.github.com>
2024-03-29 09:38:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const btn = document.getElementById('btn');
 
+btn.addEventListener('click',()=>{
+    const particles =[];
+    const color = randomColor();
+
+    const particle = document.createElement('span');
+    particle.classList.add('particle', 'move');
+
+    const { x, y} = randomLocation();
+    particle.style.setProperty('--x', x);
+    particle.style.setProperty('--y', y);
+    particle.style.background = color;
+    btn.style.background = color;
+}) 
