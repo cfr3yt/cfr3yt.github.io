42099b50fee4b7809101e964ba52ce61cbac4f73
cfr3yt <160527793+cfr3yt@users.noreply.github.com>
2024-03-29 15:36:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const btn = document.getElementById('btn');
 
-btn.addEventListener('click',()=>{
-    const particles =[];
+btn.addEventListener('click', () => {
+    const particles = [];
     const color = randomColor();
 
     const particle = document.createElement('span');
     particle.classList.add('particle', 'move');
 
-    const { x, y} = randomLocation();
+    const { x, y } = randomLocation();
     particle.style.setProperty('--x', x);
     particle.style.setProperty('--y', y);
     particle.style.background = color;
@@ -18,38 +18,37 @@ btn.addEventListener('click',()=>{
     particles.push(particle);
 
     setTimeout(() => {
-        for(let i=0; i<100; i++) {
+        for (let i = 0; i < 100; i++) {
             const innerP = document.createElement('span');
             innerP.classList.add('particle', 'move');
-            innerP.style.transform = 'translate(${x}, ${y})';
+            innerP.style.transform = `translate(${x}, ${y})`;
 
-            const xs =Math.random() *200-100 +'px';
-            const ys =Math.random() *200-100 + 'px';
+            const xs = Math.random() * 200 - 100 + 'px';
+            const ys = Math.random() * 200 - 100 + 'px';
 
-            innerP.style.setProperty('--x', 'calc(${x} + ${xs})');
+            innerP.style.setProperty('--x', `calc(${x} + ${xs})`);
 
-            innerP.style.animationDuration = Math.random() * 300 + 200 + 'ms' ;
+            innerP.style.animationDuration = Math.random() * 300 + 200 + 'ms';
             innerP.style.background = color;
 
             btn.appendChild(innerP);
-            particles.push(innerP) 
+            particles.push(innerP);
         }
-        setTimeout(()=> {
-            particles.forEach(particle=>{
+        setTimeout(() => {
+            particles.forEach(particle => {
                 particle.remove();
-            }}
-                   },1000)
-            },1000);
-        }};
+            });
+        }, 1000);
+    }, 1000);
+});
+
 function randomLocation() {
-    return{
-        x:Math.random() * window.innerWidth - window.innerWidth / 2 + 'px',
-        y:Math.random() * window.innerHeight- window.innerHeight / 2 + 'px',
-        
-    }
+    return {
+        x: Math.random() * window.innerWidth - window.innerWidth / 2 + 'px',
+        y: Math.random() * window.innerHeight - window.innerHeight / 2 + 'px'
+    };
 }
 
-function randomColor(){
-    return 'hsl(${Math.floor(Math.random()*362)}, 100%, 50%)';
+function randomColor() {
+    return `hsl(${Math.floor(Math.random() * 362)}, 100%, 50%)`;
 }
-    
