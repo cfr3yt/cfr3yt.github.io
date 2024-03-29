7a178d137cc3731d95c4b72c0c75559751dea61f
cfr3yt <160527793+cfr3yt@users.noreply.github.com>
2024-03-29 10:44:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,4 +12,45 @@ btn.addEventListener('click',()=>{
     particle.style.setProperty('--y', y);
     particle.style.background = color;
     btn.style.background = color;
+
+    btn.appendChild(particle);
+
+    particles.push(particle);
+
+    setTimeout(() => {
+        for(Let i=0; i<100; i++) {
+            const innerP = document.createElement('span');
+            innerP.classList.add('particle', 'move');
+            innerP.style.transform = 'translate(${x}, ${y})';
+
+            const xs =math.random() *200-100 +'px';
+            const ys =math.random() *200-100 + 'px';
+
+            innerP.style.setProperty('--x', 'calc(${x} + ${xs})');
+
+            innerP.style.animationDuration = Math.random() * 300 + 200 + 'ms' ;
+            innerP.style.background = color;
+
+            btn.appenChild(innerP);
+            particles.push(innerP) 
+        }
+        setTimeout(()=> {
+            particles.forEach(particle=>{
+                particle.remove();
+            }}
+                   },1000)
+            },1000);
+        }};
+function randomLocation() {
+    return{
+        x:Math.random() * window.innerWidth - window.innerWidth / 2 + 'px',
+        y:Math.random() * window.innderHeight- window.innerHeight / 2 + 'px',
+        
+    }
+}
+
+function randomColor(){
+    return`hsl($(Math.floor(Math.random()*362)},
+    100%, 50%)`;
+    
 }) 
